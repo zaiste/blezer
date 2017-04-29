9df58f6e10312861537af75bbd6e233882f7a25e
Zaiste <oh@zaiste.net>
2017-04-29 21:54:08 +0200

    tests: enqueue a job on default queue

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Job = require('../lib/job');
+const Queue = require('../lib/queue');
 
 beforeEach(() => {});
 
@@ -10,6 +11,15 @@ describe('Job', () => {
     const job = await new Job('default', 'BooWorker', [1, 'arg1', true]);
 
     expect(job.queue).to.be.equal('default');
+    expect(job.task).to.be.equal('BooWorker');
+    expect(job.args).to.be.an('array');
+  });
+
+  it('enque a job', async () => {
+    const queue = new Queue();
+    const job = await queue.enqueue('BooWorker', [1, 'arg1', true]);
+
+    expect(job.queue).to.be.equal('blezer:queues:default');
     expect(job.task).to.be.equal('BooWorker');
     expect(job.args).to.be.an('array');
   });
