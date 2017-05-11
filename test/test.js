dd66a66ca78d3b89860e1152d284df98c9b41d42
Zaiste <oh@zaiste.net>
2017-05-11 21:45:57 +0200

    feature: pass `options` via `enqueue` helper

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 const Job = require('../lib/job');
 const Queue = require('../lib/queue');
+const { enqueue } = require('../lib/helpers');
 
 beforeEach(() => {});
 
@@ -15,11 +16,19 @@ describe('Job', () => {
     expect(job.args).to.be.an('array');
   });
 
-  it('enque a job', async () => {
+  it('enque a job via Queue', async () => {
     const queue = new Queue();
     const job = await queue.enqueue('BooWorker', [1, 'arg1', true]);
 
     expect(job.queue).to.be.equal('blezer:queues:default');
+    expect(job.task).to.be.equal('BooWorker');
+    expect(job.args).to.be.an('array');
+  });
+
+  it('enque a job using a helper', async () => {
+    const job = await enqueue('BooWorker', [1, 'arg1', true], { name: 'high' });
+
+    expect(job.queue).to.be.equal('blezer:queues:high');
     expect(job.task).to.be.equal('BooWorker');
     expect(job.args).to.be.an('array');
   });
